2c66c05edbaee943897dadaf72f2f79955a2394e
sidicodet <121307234+sidicodet@users.noreply.github.com>
2023-05-14 10:04:33 +0100

    index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import { getRandomSeed } from "lib/seeds";
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
-export const appName = "Paint by Text";
+export const appName = "Paint by Text AI";
 export const appSubtitle = "Edit your photos using written instructions, with the help of an AI.";
 export const appMetaDescription = "Edit your photos using written instructions, with the help of an AI.";
 
@@ -112,7 +112,7 @@ export default function Home() {
         <meta name="description" content={appMetaDescription} />
         <meta property="og:title" content={appName} />
         <meta property="og:description" content={appMetaDescription} />
-        <meta property="og:image" content="https://paintbytext.chat/opengraph.jpg" />
+        <meta property="og:image" content="https://pbto.vercel.app/opengraph.jpg" />
       </Head>
 
       <main className="container max-w-[700px] mx-auto p-5">
